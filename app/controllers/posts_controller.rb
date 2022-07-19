c4178e806dd1abcf185d0a1cdc391b368cac6b67
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-07-19 16:51:19 -0400

    commit tree dfad8d51c7e0271cdedccbef7a9e7a30bf8b4e0f

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_baddata_response
+  
   def show
     post = Post.find(params[:id])
     
@@ -9,12 +10,16 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
   private
+
+  def handle_baddata_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 
   def post_params
     params.permit(:category, :content, :title)
